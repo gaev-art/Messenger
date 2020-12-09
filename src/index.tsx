3abe3f670d1f17459656b338c41d81545ad88bc2
gaev-art <forsakensr@gmail.com>
2020-12-09 21:12:24 +0300

    predeploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import {render} from 'react-dom'
 import './index.css'
-import * as serviceWorker from './serviceWorker'
+import React from 'react'
+import {App} from './components/App'
+import {render} from 'react-dom'
+import {HashRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {store} from './redux/store'
-import {HashRouter} from 'react-router-dom'
-import {App} from './components/App'
+import * as serviceWorker from './serviceWorker'
 
 
 render(
@@ -17,7 +17,5 @@ render(
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister()
